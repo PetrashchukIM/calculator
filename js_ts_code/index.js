d3eb336024cea3cb91524371a325908c8158fb40
PetrashchukIM <petrashchukim@gmail.com>
2024-08-08 22:59:12 +0300

    add new logic

--- js_ts_code/index.js
+++ js_ts_code/index.js
@@ -1,8 +1,22 @@
 var calculator = document.getElementsByClassName("calculator")[0];
 var keys = calculator.getElementsByClassName("calculator__keys")[0];
+var display = document.getElementsByClassName("calculator__display")[0];
 keys.addEventListener("click", function (event) {
     var target = event.target;
     if (target.matches("button")) {
-        console.log(target.innerText.split(/\s+/).join(''));
+        var action = target.dataset.action;
+        var keyContent = target.innerText.split(/\s+/).join("");
+        var displayedNum = display.innerText.split(/\s+/).join("");
+        if (!action) {
+            if (displayedNum === "0") {
+                display.textContent = keyContent;
+            }
+            else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+        console.log(action);
+        console.log(keyContent);
+        console.log(displayedNum);
     }
 });
--- js_ts_code/index.ts
+++ js_ts_code/index.ts
@@ -1,9 +1,23 @@
-const calculator:Element = document.getElementsByClassName("calculator")[0];
-const keys:Element = calculator.getElementsByClassName("calculator__keys")[0];
+const calculator = document.getElementsByClassName("calculator")[0] as HTMLElement;
+const keys = calculator.getElementsByClassName("calculator__keys")[0] as HTMLElement;
+const display = document.getElementsByClassName("calculator__display")[0] as HTMLElement;
 
-keys.addEventListener("click",  (event: MouseEvent) => {
+keys.addEventListener("click", (event: MouseEvent) => {
     const target = event.target as HTMLElement;
-    if (target.matches("button")){
-        console.log(target.innerText.split(/\s+/).join(''));
+    if (target.matches("button")) {
+        const action = target.dataset.action;
+        const keyContent = target.innerText.split(/\s+/).join("");
+        const displayedNum = display.innerText.split(/\s+/).join("");
+        if (!action) {
+            if (displayedNum === "0") {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        console.log(action);
+        console.log(keyContent);
+        console.log(displayedNum);
     }
 });
